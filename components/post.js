579ce1b83c099242aea9d64f197cc358a79355e5
Timo Clasen <timo@codeatelier.com>
2020-12-06 13:45:42 +0100

    Remove styling

--- components/post.js
+++ components/post.js
@@ -15,35 +15,6 @@ function Post({ alt, date, image, title, url }) {
                 <h2>{title}</h2>
                 <h4>{date}</h4>
             </div>
-            <style jsx>{`
-                .container {
-                    cursor: pointer;
-                    height: 453px;
-                    margin-bottom: 48px;
-                }
-                a {
-                    border-bottom: none;
-                }
-                a:hover {
-                    border-bottom: none;
-                }
-                .text {
-                    margin-top: -160px;
-                    padding: 24px;
-                    position: absolute;
-                }
-                h2 {
-                    color: white;
-                    font-size: 24px;
-                    margin-bottom: 0;
-                }
-                h4 {
-                    color: rgba(255, 255, 255, 0.8);
-                    font-size: 16px;
-                    font-weight: 500;
-                    margin-top: 8px;
-                }
-            `}</style>
         </div>
     );
 }
