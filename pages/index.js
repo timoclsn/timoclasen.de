aa9b98b3c1ad4d64b8f1b06223f32fecff0a6693
Timo Clasen <timo@codeatelier.com>
2020-12-11 20:39:56 +0100

    Change Test Content

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,28 @@
+import Image from 'next/image';
 import Layout from '../components/layout';
-import Post from '../components/post';
-import { fetchEntries } from '../lib/contentful.js';
+import { fetchEntries } from '../lib/api';
 
-export default function Home({ posts }) {
+export default function Home({ homepage }) {
     return (
         <Layout>
-            <div className={'text-5xl text-red-900'}>Test</div>
-            {posts.length > 0
-                ? posts.map((p) => (
-                      <Post
-                          alt={p.fields.alt}
-                          date={p.fields.date}
-                          key={p.fields.title}
-                          image={p.fields.image}
-                          title={p.fields.title}
-                          url={p.fields.url}
-                      />
-                  ))
-                : null}
+            <div className={'text-5xl text-red-900'}>
+                {homepage[0].fields.title}
+            </div>
+            <div>{homepage[0].fields.about}</div>
+            <Image
+                src={`https:${homepage[0].fields.profileImage.fields.file.url}`}
+                width={500}
+                height={500}
+            />
         </Layout>
     );
 }
 
 export async function getStaticProps() {
-    const posts = await fetchEntries();
+    const homepage = await fetchEntries();
     return {
         props: {
-            posts
+            homepage
         }
     };
 }
