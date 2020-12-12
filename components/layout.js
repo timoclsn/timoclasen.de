acbbd477a94a2decea3abd8dc903e63fd3f10ac7
Timo Clasen <timo@codeatelier.com>
2020-12-12 19:32:40 +0100

    Update Title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <div>
             <Head>
-                <title>Timo Clasen Website Test</title>
+                <title>Timo Clasen Website Test Test</title>
             </Head>
             <div>{children}</div>
         </div>
