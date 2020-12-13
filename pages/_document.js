0de53aff4aaa189abe22a2a0db26c0ad195545e3
Timo Clasen <timo@codeatelier.com>
2020-12-13 14:04:35 +0100

    Add lang attribute to html

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html className={'font-normal'}>
+            <Html className={'font-normal'} lang="de">
                 <Head />
                 <body>
                     <Main />
