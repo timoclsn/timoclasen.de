fc01eb25a99f45e90819c96c95ed3a96beb3ff66
Timo Clasen <timo@codeatelier.com>
2020-12-13 13:33:29 +0100

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     purge: ['./pages/**/*.js', './components/**/*.js'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans]
+            }
+        }
     },
     variants: {
         extend: {}
