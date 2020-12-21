1e92dfac9534f0620c0a7883894339dd17018e1e
Timo Clasen <timo@codeatelier.com>
2020-12-21 18:15:28 +0100

    Update Semantic

--- components/navigation.js
+++ components/navigation.js
@@ -3,17 +3,17 @@ import Link from 'next/link';
 export default function Navigation({ name, profession }) {
     return (
         <header className={'flex justify-start mb-16'}>
-            <Link href="/">
-                <a
-                    className={
-                        'hover:text-highlight dark:hover:text-highlight-dark'
-                    }>
-                    <div className={'text-xl font-bold'}>
-                        <h1>{name}</h1>
-                    </div>
-                    <div className={'text-sm'}>{profession}</div>
-                </a>
-            </Link>
+            <nav>
+                <Link href="/">
+                    <a
+                        className={
+                            'hover:text-highlight dark:hover:text-highlight-dark'
+                        }>
+                        <div className={'text-xl font-bold'}>{name}</div>
+                        <div className={'text-sm'}>{profession}</div>
+                    </a>
+                </Link>
+            </nav>
         </header>
     );
 }
