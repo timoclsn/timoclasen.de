39e1f8c51b6a9212cf469d1ff0dbc5614445d45d
Timo Clasen <timo@codeatelier.com>
2020-12-21 16:38:50 +0100

    Change Route Name

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <footer className={'flex justify-end mt-16'}>
-            <Link href="/legal">
+            <Link href="/impressum">
                 <a
                     className={
                         'text-sm hover:text-highlight dark:hover:text-highlight-dark'
