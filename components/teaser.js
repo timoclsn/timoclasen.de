318340c182de6580404a24d63724c5c8a67df07a
Timo Clasen <timo@codeatelier.com>
2020-12-21 18:13:08 +0100

    Update Typo

--- components/teaser.js
+++ components/teaser.js
@@ -1,7 +1,7 @@
 export default function Teaser({ text }) {
     return (
         <div
-            className={'prose dark:prose-dark prose-2xl'}
+            className={'prose dark:prose-dark prose lg:prose-lg xl:prose-xl'}
             dangerouslySetInnerHTML={{ __html: text }}></div>
     );
 }
--- components/textblock.js
+++ components/textblock.js
@@ -1,7 +1,7 @@
 export default function Textblock({ text }) {
     return (
         <article
-            className={'prose dark:prose-dark prose-sm'}
+            className={'prose dark:prose-dark prose lg:prose-lg xl:prose-xl'}
             dangerouslySetInnerHTML={{ __html: text }}></article>
     );
 }
