9e7dc92091d75d44957981cae3af22c4f0bcdb86
Timo Clasen <timo@codeatelier.com>
2020-12-23 14:18:54 +0100

    Change Font Display

--- components/layout.js
+++ components/layout.js
@@ -52,7 +52,7 @@ export default function Layout({
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
                     rel="preload"
-                    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=optional"
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
                     rel="stylesheet"
                 />
             </Head>
