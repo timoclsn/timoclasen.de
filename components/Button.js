ef903325c42d51a06ab1a0033fee7846cc5a212d
Timo Clasen <timo@codeatelier.com>
2020-12-28 08:40:30 +0100

    Update Button Font Weight

--- components/Button.js
+++ components/Button.js
@@ -5,7 +5,7 @@ export default function Button({ Icon, text, href, secondary }) {
                 !secondary
                     ? 'bg-highlight dark:bg-highlight-dark text-light dark:text-light hover:bg-opacity-90'
                     : 'border-dark dark:border-light hover:bg-dark dark:hover:bg-light hover:bg-opacity-10 border-2'
-            } block w-full p-3  rounded-full cursor-pointer`}
+            } block w-full p-3  rounded-full cursor-pointer font-bold`}
             href={href}>
             <div className={'flex justify-center items-center space-x-2'}>
                 <Icon size={20} />
