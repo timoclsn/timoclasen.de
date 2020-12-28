75e57b6adc04a45a701b70fd0f486944dc7becd4
Timo Clasen <timo@codeatelier.com>
2020-12-28 08:08:30 +0100

    Add Cutom Teaser Component

--- components/Teaser.js
+++ components/Teaser.js
@@ -1,9 +1,7 @@
 export default function Teaser({ text }) {
     return (
         <div
-            className={
-                'prose dark:prose-dark text-2xl sm:text-3xl md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight xl:text-6xl xl:leading-tight max-w-none'
-            }
+            className={'text-teaser'}
             dangerouslySetInnerHTML={{ __html: text }}></div>
     );
 }
