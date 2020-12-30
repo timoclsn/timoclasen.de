2966f9e9d4561453ff0c3a9f3af42c4b69654ab9
Timo Clasen <goebeltimo@gmail.com>
2020-12-30 12:06:16 +0100

    Test LCP

--- components/WidgetImage.js
+++ components/WidgetImage.js
@@ -1,18 +1,18 @@
 import Image from 'next/image';
 
-export default function WidgetImage({ url, description, priority }) {
+export default function WidgetImage({ url, description }) {
     return (
         <>
             <div className={'relative w-full h-96 sm:h-full'}>
-                <Image
+                {/* <Image
                     src={url}
                     layout={'fill'}
                     objectFit="cover"
                     objectPosition="center center"
                     alt={description}
                     quality={90}
-                    priority={priority}
-                />
+                    sizes="(min-width: 640px) 50vw, 100vw"
+                /> */}
             </div>
         </>
     );
