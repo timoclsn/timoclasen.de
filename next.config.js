ee65beb1f94a622c2a4919f4ba54d9464a689ea4
Timo Clasen <goebeltimo@gmail.com>
2020-12-31 09:08:03 +0100

    Add Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({
     images: {
         domains: ['images.ctfassets.net']
     }
-};
+});
