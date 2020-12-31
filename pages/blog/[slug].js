a501ba0ae75e1b65154974e0488cd58a83f3ee30
Timo Clasen <goebeltimo@gmail.com>
2020-12-31 14:20:21 +0100

    Add blog post SEO information

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,42 +2,59 @@ import Layout from '../../components/Layout';
 import TextPost from '../../components/TextPost';
 import ContactWidget from '../../components/ContactWidget';
 import { queryContent } from '../../lib/content';
+import { NextSeo, ArticleJsonLd } from 'next-seo';
 
-export default function Blog(props) {
+export default function BlogPost(props) {
+    const date = new Date(props.blogPost.date).toISOString();
     return (
-        <Layout
-            title={props.title}
-            description={props.description}
-            previewImage={props.previewImage}
-            slug={props.slug}>
-            <TextPost text={props.blogPost.text} key={props.blogPost.slug} />
-            <ContactWidget text={props.contact} />
-        </Layout>
+        <>
+            <NextSeo
+                openGraph={{
+                    type: 'article',
+                    article: {
+                        publishedTime: date
+                    }
+                }}
+            />
+            <ArticleJsonLd
+                authorName="Timo Clasen"
+                dateModified={date}
+                datePublished={date}
+                description={props.blogPost.summary}
+                images={[props.blogPost.previewImage.url]}
+                publisherLogo="/favicons/android-chrome-192x192.png"
+                publisherName="Timo Clasen"
+                title={props.blogPost.title}
+                url={`https://timoclasen.de/blog/${props.blogPost.slug}`}
+            />
+            <Layout
+                title={props.blogPost.title}
+                description={props.blogPost.summary}
+                previewImage={props.blogPost.previewImage}
+                slug={`blog/${props.blogPost.slug}`}>
+                <TextPost text={props.blogPost.text} />
+                <ContactWidget text={props.contact} />
+            </Layout>
+        </>
     );
 }
 
 export async function getStaticProps({ params }) {
     const response = await queryContent(
         `{
-            page: pageCollection(where: {slug: "blog"}, limit: 1) {
-                items {
-                    title
-                    slug
-                    description
-                    previewImage {
-                        url
-                        description
-                    }
-                }
-            }
             blogPost: blogPostCollection(where: {slug: "${params.slug}"}, limit: 1) {
                 items {
                     title
                     slug
+                    previewImage {
+                        url
+                        description
+                    }
                     date
                     author {
                         name
                     }
+                    summary
                     text
                 }
             }
@@ -49,16 +66,11 @@ export async function getStaticProps({ params }) {
         }`
     );
 
-    const page = response.data.page.items[0];
     const blogPost = response.data.blogPost.items[0];
     const contactText = response.data.contactSnippet.items[0].content;
 
     return {
         props: {
-            title: page.title,
-            description: page.description,
-            previewImage: page.previewImage,
-            slug: page.slug,
             blogPost,
             contact: contactText
         }
