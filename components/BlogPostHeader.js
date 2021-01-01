5d54df486f175d37191685d05174805c27524b5a
Timo Clasen <goebeltimo@gmail.com>
2021-01-01 14:38:49 +0100

    Update blogpost header responsive handling

--- components/BlogPostHeader.js
+++ components/BlogPostHeader.js
@@ -15,19 +15,19 @@ export default function BlogPostHeader({ title, summary, date, author, text }) {
             <div className={'max-w-prose mx-auto'}>
                 <div
                     className={
-                        'w-full flex flex-wrap justify-between text-sm md:text-base'
+                        'w-full flex flex-wrap justify-between text-xs sm:text-sm md:text-base'
                     }>
-                    <div className={'flex items-center space-x-2 mr-4 mb-2'}>
+                    <div className={'flex items-center space-x-2 mr-2 mb-2'}>
                         <Image
                             src={author.image.url}
-                            width={24}
-                            height={24}
+                            width={32}
+                            height={32}
                             layout="fixed"
                             alt={author.image.description}
-                            quality={90}
+                            quality={75}
                             className={'rounded-full'}
                         />
-                        <p className={'opacity-70 uppercase'}>
+                        <p className={'opacity-70 uppercase flex-1'}>
                             {author.username ? (
                                 <a
                                     className={
@@ -38,17 +38,18 @@ export default function BlogPostHeader({ title, summary, date, author, text }) {
                                     rel="noopener noreferrer">
                                     {author.name}
                                     <Twitter
-                                        className={'inline ml-1'}
+                                        className={'inline mx-2'}
                                         size={16}
                                     />
                                 </a>
                             ) : (
                                 author.name
                             )}
-                            {` | ${format(parseISO(date), 'dd. MMMM yyyy', {
-                                locale: de
-                            })}`}
-                            {}
+                            <span className={'whitespace-nowrap'}>
+                                {format(parseISO(date), 'dd. MMMM yyyy', {
+                                    locale: de
+                                })}
+                            </span>
                         </p>
                     </div>
                     <div
