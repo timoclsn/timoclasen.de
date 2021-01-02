12386d6e8dce70fc5335e589678a2064710b779b
Timo Clasen <goebeltimo@gmail.com>
2021-01-02 11:34:09 +0100

    Fix preview mode

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -7,13 +7,7 @@ export default async function preview(req, res) {
         return res.status(401).json({ message: 'Invalid token' });
     }
 
-    if (!post) {
-        return res.status(401).json({ message: 'Invalid slug' });
-    }
-
-    res.setPreviewData({});
-
-    const post = await queryContent(
+    const response = await queryContent(
         `{
             blogPost: blogPostCollection(where: {slug: "${slug}"}, limit: 1) {
                 items {
@@ -23,6 +17,14 @@ export default async function preview(req, res) {
             }
         }`
     );
+
+    const post = response.data.blogPost.items[0];
+
+    if (!post) {
+        return res.status(401).json({ message: 'Invalid slug' });
+    }
+
+    res.setPreviewData({});
 
     res.redirect(`/blog/${post.slug}`);
     res.end();
