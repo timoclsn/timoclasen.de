57dde4407061ecf7f6d2f9bbca9a7880b7c8a9db
Timo Clasen <goebeltimo@gmail.com>
2021-01-02 08:22:46 +0100

    Fix widget text link position

--- components/WidgetText.js
+++ components/WidgetText.js
@@ -7,23 +7,25 @@ export default function WidgetText({ title, text, href }) {
         <>
             <div
                 className={
-                    'relative flex flex-col px-6 py-12 xl:px-12 xl:py-20'
+                    'h-full flex flex-col justify-between px-6 py-12 xl:px-12 xl:py-20'
                 }>
-                <h2
-                    className={
-                        'font-bold text-xl md:text-2xl lg:text-3xl mb-4'
-                    }>
-                    {title}
-                </h2>
-                <div className={'flex flex-col space-y-4'}>
-                    <ReactMarkdown>{text}</ReactMarkdown>
+                <div>
+                    <h2
+                        className={
+                            'font-bold text-xl md:text-2xl lg:text-3xl mb-4'
+                        }>
+                        {title}
+                    </h2>
+                    <div className={'flex flex-col space-y-4'}>
+                        <ReactMarkdown>{text}</ReactMarkdown>
+                    </div>
                 </div>
                 {href && (
-                    <div className={'flex justify-end -mb-6 xl:-mb-12'}>
+                    <div className={'flex justify-end mt-4 -mb-6 xl:-mb-12'}>
                         <Link href={href}>
                             <a
                                 className={
-                                    'flex justify-center items-center space-x-2 mt-4 hover:opacity-80'
+                                    'flex items-center space-x-2 hover:opacity-80'
                                 }>
                                 <ArrowRight size={24} />
                                 <div className={'font-bold'}>mehr</div>
