5ae30aec3718647a6279ee669d25a0ba8090317c
Timo Clasen <goebeltimo@gmail.com>
2021-01-03 10:16:23 +0100

    Revert "Test preview mode var"
    This reverts commit 3e99bac76fd5c6be8087493435186f4a62fcbedd.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -52,8 +52,6 @@ export default function BlogPost(props) {
 }
 
 export async function getStaticProps({ params, preview = false }) {
-    console.log(`Preview: ${preview}`);
-
     const response = await queryContent(
         `{
             blogPost: blogPostCollection(where: {slug: "${params.slug}"}, limit: 1, preview: false) {
