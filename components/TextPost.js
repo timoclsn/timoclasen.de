a01a6b83ebef70e0fbd81d55328c0342367778c0
Timo Clasen <goebeltimo@gmail.com>
2021-01-04 07:44:44 +0100

    Revert "Change syntax highlighter theme"
    This reverts commit e5770c72fe072e6e11435d20634f3b540b3cad8a.

--- components/TextPost.js
+++ components/TextPost.js
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
-import style from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
+import style from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
 
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 
