76789e7b345dc43b1eef5c836f115bca06da7f6c
Timo Clasen <goebeltimo@gmail.com>
2021-01-26 20:36:57 +0100

    Calculate distance this year

--- pages/api/running.js
+++ pages/api/running.js
@@ -1,23 +1,18 @@
-import fetcher from '@/lib/fetcher';
-
-const clientID = process.env.STRAVA_CLIENT_ID;
-const clientSecret = process.env.STRAVA_CLIENT_SECRET;
-const refreshToken = process.env.STRAVA_REFRESH_TOKEN;
+import { getActivities } from '@/lib/strava';
 
 export default async (_, res) => {
-    const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
-    const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`;
+    const activites = await getActivities();
 
-    const accessData = await fetcher(callRefresh, {
-        method: 'POST'
-    });
-
-    const data = await fetcher(callActivities + accessData.access_token);
+    const distanceThisYear = activites.reduce((acc, activity) => {
+        return acc + activity.distance / 1000;
+    }, 0);
 
     res.setHeader(
         'Cache-Control',
         'public, s-maxage=600, stale-while-revalidate=1200'
     );
 
-    return res.status(200).json(data);
+    return res.status(200).json({
+        distanceThisYear: `${Math.round(distanceThisYear)} km`
+    });
 };
