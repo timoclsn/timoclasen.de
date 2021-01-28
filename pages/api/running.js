d259ec2f29b7db7fffbdafe4e5bac9b1060b7fff
Timo Clasen <goebeltimo@gmail.com>
2021-01-28 18:54:31 +0100

    Expand running api

--- pages/api/running.js
+++ pages/api/running.js
@@ -1,11 +1,30 @@
-import { getActivities } from '@/lib/strava';
+import { format, parseISO } from 'date-fns';
+import { de } from 'date-fns/locale';
+
+import {
+    formatSpeed,
+    formatTime,
+    getActivities,
+    roundDistance
+} from '@/lib/strava';
 
 export default async (_, res) => {
     const activites = await getActivities();
 
     const distanceThisYear = activites.reduce((acc, activity) => {
-        return acc + activity.distance / 1000;
+        return acc + activity.distance;
     }, 0);
+
+    const lastRun = activites.reduce((lastRun, activity) => {
+        if (!lastRun.start_date) {
+            return activity;
+        }
+
+        return parseISO(activity.start_date).getTime() >
+            parseISO(lastRun.start_date).getTime()
+            ? activity
+            : lastRun;
+    }, {});
 
     res.setHeader(
         'Cache-Control',
@@ -13,6 +32,18 @@ export default async (_, res) => {
     );
 
     return res.status(200).json({
-        distanceThisYear: `${Math.round(distanceThisYear)} km`
+        thisYear: {
+            distance: `${Math.round(distanceThisYear / 1000)} km`
+        },
+        lastRun: {
+            distance: `${roundDistance(lastRun.distance / 1000)} km`,
+            date: format(parseISO(lastRun.start_date), 'dd. MMMM yyyy', {
+                locale: de
+            }),
+            time: formatTime(lastRun.moving_time),
+            avgSpeed: formatSpeed(lastRun.average_speed),
+            avgHeartrate: `${Math.round(lastRun.average_heartrate)} bpm`,
+            map: lastRun.map.summary_polyline
+        }
     });
 };
