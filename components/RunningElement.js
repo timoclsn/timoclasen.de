db983bde9c9a3dac4f8bbb9789de1b93ab8c1834
Timo Clasen <goebeltimo@gmail.com>
2021-01-29 19:46:16 +0100

    Update styling

--- components/RunningElement.js
+++ components/RunningElement.js
@@ -1,14 +1,16 @@
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 export default function RunningElement({ Icon, text }) {
     return (
-        <div className={'flex space-x-2 mb-4 content-center'}>
-            {text ? (
-                <Icon />
-            ) : (
-                <Skeleton circle={true} height={22} width={22} />
-            )}
-            <p>{text ? text : <Skeleton width={200} />}</p>
-        </div>
+        <SkeletonTheme color="#8D8D8D" highlightColor="#B5B5B5">
+            <div className={'flex items-center space-x-4 mb-4'}>
+                {text ? (
+                    <Icon size={24} />
+                ) : (
+                    <Skeleton circle={true} height={20} width={20} />
+                )}
+                <p>{text ? text : <Skeleton width={200} />}</p>
+            </div>
+        </SkeletonTheme>
     );
 }
