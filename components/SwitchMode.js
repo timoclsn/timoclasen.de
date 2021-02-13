45ca6a51732f87c18134659cf6652b618455edbb
Timo Clasen <goebeltimo@gmail.com>
2021-02-13 11:31:29 +0100

    Add tooltip to theme switch

--- components/SwitchMode.js
+++ components/SwitchMode.js
@@ -21,7 +21,8 @@ export default function SwitchMode() {
             className={
                 'flex items-center justify-center text-highlight dark:text-highlight-dark bg-dark dark:bg-light bg-opacity-10 dark:bg-opacity-10 rounded-xl h-8 w-8 focus:outline-none'
             }
-            onClick={handleThemeChange}>
+            onClick={handleThemeChange}
+            title={`Farbschema zu ${darkMode ? 'hell' : 'dunkel'} welchseln`}>
             {mounted && (darkMode ? <Sun size={16} /> : <Moon size={16} />)}
         </button>
     );
