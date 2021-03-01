914503cc0d2e4d2f94b8f828bf1bdb7fc0e3735c
Timo Clasen <goebeltimo@gmail.com>
2021-03-01 20:05:02 +0100

    Fetch and parse rss feeds

--- scripts/podcasts/index.js
+++ scripts/podcasts/index.js
@@ -1,11 +1,60 @@
-const fs = require('fs/promises');
-const convert = require('xml-js');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const parser = require('fast-xml-parser');
 
 (async () => {
-    const subsXML = await fs.readFile('./scripts/podcasts/subscriptions.opml');
-    const subsJSON = convert.xml2json(subsXML, {
-        compact: true,
-        spaces: 2
-    });
-    console.log(subsJSON);
+    const subsXML = fs
+        .readFileSync('./scripts/podcasts/subscriptions.opml')
+        .toString();
+
+    const subsJSObj = parser.parse(
+        subsXML,
+        {
+            attributeNamePrefix: '',
+            attrNodeName: false,
+            ignoreAttributes: false
+        },
+        true
+    ).opml.body.outline;
+
+    const data = await Promise.all(
+        subsJSObj.flatMap(async (podcast) => {
+            let podcastObj = {
+                titel: podcast.title,
+                feedUrl: podcast.xmlUrl
+            };
+
+            const response = await fetch(podcast.xmlUrl);
+            if (response.ok) {
+                const podcastXML = await response.text();
+                try {
+                    const podcastJSObj = parser.parse(
+                        podcastXML,
+                        {
+                            attributeNamePrefix: '',
+                            attrNodeName: false,
+                            ignoreAttributes: false
+                        },
+                        true
+                    ).rss.channel;
+
+                    podcastObj.title = podcastJSObj.title;
+                    podcastObj.description = podcastJSObj.description;
+                    podcastObj.website = podcastJSObj.link;
+                    podcastObj.image = podcastJSObj.image
+                        ? podcastJSObj.image.url
+                        : podcastJSObj['itunes:image'].href;
+                } catch (e) {
+                    console.log(`❌ ${podcast.title} (${podcast.xmlUrl})`);
+                    return [];
+                }
+            } else {
+                console.log(`❌ ${podcast.title} (${podcast.xmlUrl})`);
+                return [];
+            }
+            return podcastObj;
+        })
+    );
+
+    console.log(data);
 })();
