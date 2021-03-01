99357901454e07d832aeefc408803a765c3e5c22
Timo Clasen <goebeltimo@gmail.com>
2021-03-01 20:47:04 +0100

    Download cover images

--- scripts/podcasts/index.js
+++ scripts/podcasts/index.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const parser = require('fast-xml-parser');
 
+const coversDir = './public/podcasts';
+
+if (fs.existsSync(coversDir)) {
+    fs.rmdirSync(coversDir, { recursive: true });
+}
+
+fs.mkdirSync(coversDir);
+
 (async () => {
     const subsXML = fs
         .readFileSync('./scripts/podcasts/subscriptions.opml')
@@ -41,9 +49,23 @@ const parser = require('fast-xml-parser');
                     podcastObj.title = podcastJSObj.title;
                     podcastObj.description = podcastJSObj.description;
                     podcastObj.website = podcastJSObj.link;
-                    podcastObj.image = podcastJSObj.image
+
+                    const randomString = Math.random()
+                        .toString(36)
+                        .substring(7);
+
+                    const imageUrl = podcastJSObj.image
                         ? podcastJSObj.image.url
                         : podcastJSObj['itunes:image'].href;
+
+                    const imageResponse = await fetch(imageUrl);
+                    const buffer = await imageResponse.buffer();
+                    fs.writeFile(
+                        `${coversDir}/${randomString}.jpg`,
+                        buffer,
+                        () => {}
+                    );
+                    podcastObj.image = `/public/podcasts/${randomString}.jpg`;
                 } catch (e) {
                     console.log(`❌ ${podcast.title} (${podcast.xmlUrl})`);
                     return [];
