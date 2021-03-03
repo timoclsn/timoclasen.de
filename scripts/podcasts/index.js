de8152e9a1eafac0d37367478b8cd2f9a4926cb2
Timo Clasen <goebeltimo@gmail.com>
2021-03-03 19:51:11 +0100

    Update error handling

--- scripts/podcasts/index.js
+++ scripts/podcasts/index.js
@@ -26,65 +26,63 @@ fs.mkdirSync(coversDir);
     ).opml.body.outline;
 
     const podcasts = await Promise.all(
-        subsJSObj.map(async (podcast) => {
-            let podcastObj = {};
+        subsJSObj
+            .map(async (podcast) => {
+                let podcastObj = {};
 
-            const response = await fetch(podcast.xmlUrl);
-            if (response.ok) {
-                const podcastXML = await response.text();
-                try {
-                    const podcastJSObj = parser.parse(
-                        podcastXML,
-                        {
-                            attributeNamePrefix: '',
-                            attrNodeName: false,
-                            ignoreAttributes: false
-                        },
-                        true
-                    ).rss.channel;
+                const response = await fetch(podcast.xmlUrl);
+                if (response.ok) {
+                    const podcastXML = await response.text();
+                    try {
+                        const podcastJSObj = parser.parse(
+                            podcastXML,
+                            {
+                                attributeNamePrefix: '',
+                                attrNodeName: false,
+                                ignoreAttributes: false
+                            },
+                            true
+                        ).rss.channel;
 
-                    podcastObj.title = podcastJSObj.title;
-                    podcastObj.feedUrl = podcastJSObj.xmlUrl;
-                    podcastObj.description = podcastJSObj.description;
-                    podcastObj.website = podcastJSObj.link;
-                    podcastObj.hosts = podcastJSObj['itunes:author'];
-                    podcastObj.category =
-                        podcastJSObj['media:category'] ||
-                        podcastJSObj['itunes:category'];
+                        podcastObj.title = podcastJSObj.title;
+                        podcastObj.feedUrl = podcastJSObj.xmlUrl;
+                        podcastObj.description = podcastJSObj.description;
+                        podcastObj.website = podcastJSObj.link;
+                        podcastObj.hosts = podcastJSObj['itunes:author'];
+                        podcastObj.category =
+                            podcastJSObj['media:category'] ||
+                            podcastJSObj['itunes:category'];
 
-                    const randomString = Math.random()
-                        .toString(36)
-                        .substring(7);
+                        const randomString = Math.random()
+                            .toString(36)
+                            .substring(7);
 
-                    const imageUrl = podcastJSObj.image
-                        ? podcastJSObj.image.url
-                        : podcastJSObj['itunes:image'].href;
+                        const imageUrl = podcastJSObj.image
+                            ? podcastJSObj.image.url
+                            : podcastJSObj['itunes:image'].href;
 
-                    const imageResponse = await fetch(imageUrl);
-                    const imageBuffer = await imageResponse.buffer();
-                    const filename = `${coversDir}/cover-${randomString}.jpg`;
-                    fs.writeFile(filename, imageBuffer, () => {});
-                    podcastObj.image = filename;
-                } catch (e) {
+                        const imageResponse = await fetch(imageUrl);
+                        const imageBuffer = await imageResponse.buffer();
+                        const filename = `${coversDir}/cover-${randomString}.jpg`;
+                        fs.writeFile(filename, imageBuffer, () => {});
+                        podcastObj.image = `/podcasts/cover-${randomString}.jpg`;
+                    } catch (e) {
+                        console.log(
+                            `❌ (parse feed) ${podcast.title} (${podcast.xmlUrl})`
+                        );
+                        return [];
+                    }
+                } else {
                     console.log(
-                        `❌ (parse feed) ${podcast.title} (${podcast.xmlUrl})`
+                        `❌ (fetch feed) ${podcast.title} (${podcast.xmlUrl})`
                     );
-                    return null;
+                    return [];
                 }
-            } else {
-                console.log(
-                    `❌ (fetch feed) ${podcast.title} (${podcast.xmlUrl})`
-                );
-                return null;
-            }
-            return podcastObj;
-        })
+                return podcastObj;
+            })
+            .flat()
     );
 
-    const filteredPodcasts = podcasts.filter(
-        (podcast) => podcast !== null || Object.keys(podcast).length === 0
-    );
-
-    const podcastsJSON = JSON.stringify(filteredPodcasts);
+    const podcastsJSON = JSON.stringify(podcasts);
     fs.writeFileSync('./data/podcasts.json', podcastsJSON);
 })();
