58ff27996733fedb4c12269e7c51d53c185da1ed
Timo Clasen <goebeltimo@gmail.com>
2021-03-21 13:35:46 +0100

    Refactor Button Component Styles

--- components/Button.js
+++ components/Button.js
@@ -13,28 +13,44 @@ export default function Button({
     target
 }) {
     const Tag = href ? 'a' : 'button';
-    const styles = `px-8 py-3 rounded-full cursor-pointer font-bold flex items-center justify-center space-x-2 ${
-        !secondary
-            ? 'bg-highlight dark:bg-highlight-dark text-light dark:text-light hover:bg-opacity-90 dark:hover:bg-opacity-90 disabled:opacity-50'
-            : 'border-dark dark:border-light hover:bg-dark dark:hover:bg-light hover:bg-opacity-10 dark:hover:bg-opacity-10 border-2 disabled:opacity-50'
-    } ${fullWidth && 'w-full'}`;
+    const className = [
+        'flex',
+        'items-center',
+        'justify-center',
+        'px-8',
+        'py-3',
+        'rounded-full',
+        'font-bold',
+        'space-x-2',
+        'disabled:opacity-50',
+        'focus:outline-none',
+        ...(!secondary
+            ? [
+                  'bg-highlight dark:bg-highlight-dark text-light dark:text-light hover:bg-opacity-90 dark:hover:bg-opacity-90'
+              ]
+            : [
+                  'border-dark dark:border-light hover:bg-dark dark:hover:bg-light hover:bg-opacity-10 dark:hover:bg-opacity-10 border-2'
+              ]),
+        ...(fullWidth ? ['w-full'] : ['w-full sm:w-auto'])
+    ].join(' ');
 
     return (
         <Tag
-            className={styles}
+            className={className}
             type={Tag === 'button' ? type : undefined}
+            aria-label={Tag === 'button' ? text : undefined}
             onClick={onClick}
             disabled={disabled}
-            aria-label={Tag === 'button' ? text : undefined}
             href={href}
             target={target}
             rel={target === '_blank' ? 'noopener noreferrer' : undefined}>
-            {isLoading ? (
-                <Loader size={20} className="animate-spin" />
-            ) : (
-                <Icon size={20} />
-            )}
-            <span>{text}</span>
+            {Icon &&
+                (isLoading ? (
+                    <Loader size={20} className="animate-spin" />
+                ) : (
+                    <Icon size={20} />
+                ))}
+            {text && <span>{text}</span>}
         </Tag>
     );
 }
