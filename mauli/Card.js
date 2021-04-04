f8488c4fe7c7054cb3c8e618521b8c3009876b04
Timo Clasen <goebeltimo@gmail.com>
2021-04-04 20:56:55 +0200

    Add Box Component

---
+++ mauli/Card.js
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+import Box from '@/mauli/Box';
+
+export default function Card({ children, as = 'div', className, ...props }) {
+    const Element = as;
+
+    const styles = clsx(
+        'rounded-3xl',
+        'bg-highlight dark:bg-highlight-dark',
+        className
+    );
+
+    return (
+        <Box as={Element} className={styles} {...props}>
+            {children}
+        </Box>
+    );
+}
--- pages/mauli.js
+++ pages/mauli.js
@@ -1,15 +1,23 @@
 import Button from '@/components/Button';
 import Box from '@/mauli/Box';
+import Card from '@/mauli/Card';
 import Stack from '@/mauli/Stack';
 
 export default function Mauli() {
     return (
         <Box inset={['small', 'medium', 'large']}>
-            <Stack
-                direction={['vertical', 'horizontal', 'vertical']}
-                space={['small', 'medium', 'large']}>
-                <Button text="Button 1" />
-                <Button text="Button 2" />
+            <Stack className="w-full">
+                <Stack
+                    direction={['vertical', 'horizontal', 'vertical']}
+                    space={['small', 'medium', 'large']}
+                    className="w-full">
+                    <Button text="Button 1" />
+                    <Button text="Button 2" />
+                </Stack>
+                <Stack direction="horizontal" className="w-full">
+                    <Card className="w-full">Card 1</Card>
+                    <Card className="w-full">Card 2</Card>
+                </Stack>
             </Stack>
         </Box>
     );
