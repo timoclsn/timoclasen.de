43acf55bd40b752863c37420a87117d08aeaacb1
Timo Clasen <goebeltimo@gmail.com>
2021-05-14 16:11:12 +0200

    Remove Widget Button Component

--- components/ContactWidget.js
+++ components/ContactWidget.js
@@ -1,4 +1,6 @@
-import WidgetButtons from '@/components/WidgetButtons';
+import { Linkedin, Mail, Twitter } from 'react-feather';
+
+import Button from '@/components/Button';
 import WidgetLayout from '@/components/WidgetLayout';
 import WidgetText from '@/components/WidgetText';
 
@@ -6,7 +8,30 @@ export default function ContactWidget({ text }) {
     return (
         <WidgetLayout>
             <WidgetText title="Kontakt" text={text} />
-            <WidgetButtons />
+            <div className="flex flex-col items-center justify-between w-full h-full px-6 py-12 pt-0 space-y-4 sm:space-y-0 xl:px-12 sm:pt-12 xl:py-20">
+                <Button href="mailto:timo@timoclasen.de" fullWidth>
+                    <Mail />
+                    E-Mail
+                </Button>
+                <Button
+                    variant="ghost"
+                    href="https://twitter.com/timoclsn"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    fullWidth>
+                    <Twitter />
+                    Twitter
+                </Button>
+                <Button
+                    variant="ghost"
+                    href="https://www.linkedin.com/in/timoclsn/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    fullWidth>
+                    <Linkedin />
+                    LinkedIn
+                </Button>
+            </div>
         </WidgetLayout>
     );
 }
