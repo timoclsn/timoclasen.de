4e001dca4377f7481781082aaa587e445dac2e49
Timo Clasen <goebeltimo@gmail.com>
2021-05-22 15:32:01 +0200

    Convert Centered Column to TypeScript

--- components/CenteredColumn.tsx
+++ components/CenteredColumn.tsx
@@ -1,4 +1,10 @@
-export default function CenteredColumn({ children }) {
+import { ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode;
+}
+
+export default function CenteredColumn({ children }: Props) {
     return (
         <section className="max-w-screen-xl px-6 mx-auto space-y-12 md:px-12 lg:px-24 md:space-y-16 lg:space-y-24">
             {children}
