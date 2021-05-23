d9245b38cafc9bdb795b013daf53fb9b6dd28292
Timo Clasen <goebeltimo@gmail.com>
2021-05-23 12:35:33 +0200

    Convert Text Container to TypeScript

--- components/TextContainer.tsx
+++ components/TextContainer.tsx
@@ -1,4 +1,10 @@
-export default function TextContainer({ children }) {
+import { ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode;
+}
+
+export default function TextContainer({ children }: Props) {
     return (
         <section className="mx-auto prose dark:prose-dark lg:prose-lg xl:prose-xl">
             {children}
