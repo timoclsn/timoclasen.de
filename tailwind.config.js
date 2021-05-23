4d543e3850dc6b84f40043563bee02bbcb0d4d15
Timo Clasen <goebeltimo@gmail.com>
2021-05-23 21:33:35 +0200

    Add TypeScript Support to Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
     mode: 'jit',
-    purge: ['./pages/**/*.js', './components/**/*.js'],
+    purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
+    ],
     darkMode: 'class',
     theme: {
         screens: {
