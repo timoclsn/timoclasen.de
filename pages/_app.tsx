1ae68d62c2bcb444f09f894be7737e94b8098a43
Timo Clasen <goebeltimo@gmail.com>
2021-05-24 12:50:39 +0200

    Convert App to TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import '@/styles/globals.css';
-
+import type { AppProps } from 'next/app';
 import { SkeletonTheme } from 'react-loading-skeleton';
 
-import { ThemeProvider } from '@/components/ThemeContext';
+import '@/styles/globals.css';
+import { ThemeProvider } from '../components/ThemeContext';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
             <SkeletonTheme color="#8D8D8D" highlightColor="#B5B5B5">
