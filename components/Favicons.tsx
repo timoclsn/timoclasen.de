ea6f03b3ebfa8abb952e177dfd0f41abbb889447
Timo Clasen <goebeltimo@gmail.com>
2021-06-03 16:43:28 +0200

    Update Favicons
    - Only use PNG and SVG (simpler to handle)
    - Update color so safari doesn’t add light bg in dark mode

--- components/Favicons.tsx
+++ components/Favicons.tsx
@@ -3,17 +3,18 @@ import Head from 'next/head';
 export function Favicons() {
     return (
         <Head>
-            <link rel="icon" href="/favicons/favicon.ico" />
+            <link rel="icon" type="image/png" href="/favicons/favicon.png" />
             <link
                 rel="icon"
-                href="/favicons/favicon.svg"
                 type="image/svg+xml"
+                href="/favicons/favicon.svg"
             />
             <link
                 rel="apple-touch-icon"
-                href="/favicons/apple-touch-icon.png"
+                href="/favicons/favicon-apple-touch.png"
             />
             <link rel="manifest" href="/favicons/manifest.webmanifest" />
+            <meta name="theme-color" content="#3E51F7" />
         </Head>
     );
 }
