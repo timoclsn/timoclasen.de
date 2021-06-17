931b737b58d3f384696159cf5c6439f9ab983193
Timo Clasen <goebeltimo@gmail.com>
2021-06-17 15:12:34 +0200

    Update Skeleton Handling
    Move inside text tags so height is inferred.

--- components/MediaPreview.tsx
+++ components/MediaPreview.tsx
@@ -44,44 +44,32 @@ export function MediaPreview({
                 </div>
             )}
             <div className="flex-1 overflow-hidden">
-                {byline ? (
-                    <div className="flex justify-between mb-1 space-x-2 text-xs uppercase sm:mb-2 text-highlight dark:text-highlight-dark">
-                        <div className="flex items-center space-x-2 truncate">
-                            {BylineIcon && (
-                                <BylineIcon size={16} className="flex-none" />
-                            )}
-                            <p title={byline} className="truncate">
-                                {byline}
-                            </p>
-                        </div>
-                        {favorite && <Heart size={16} className="flex-none" />}
+                <div className="flex justify-between mb-1 space-x-2 text-xs uppercase sm:mb-2 text-highlight dark:text-highlight-dark">
+                    <div className="flex items-center space-x-2 truncate">
+                        {BylineIcon && (
+                            <BylineIcon size={16} className="flex-none" />
+                        )}
+                        <p title={byline} className="truncate">
+                            {byline ? byline : <Skeleton width="200px" />}
+                        </p>
                     </div>
-                ) : (
-                    <Skeleton width="30%" height="16px" className="mb-1" />
-                )}
-                {title ? (
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        <h2
-                            className="mb-2 text-lg font-bold truncate group sm:text-xl md:text-2xl lg:text-3xl hover:text-highlight dark:hover:text-highlight-dark md:mb-4"
-                            title={title}>
-                            {no && (
-                                <span className="opacity-60 group-hover:opacity-100">
-                                    {no}.{' '}
-                                </span>
-                            )}
-                            {title}
-                        </h2>
-                    </a>
-                ) : (
-                    <Skeleton width="100%" height="32px" className="mb-4" />
-                )}
-                {subline ? (
-                    <p className="text-base md:text-md lg:text-lg xl:text-xl opacity-60 line-clamp-2">
-                        {subline}
-                    </p>
-                ) : (
-                    <Skeleton width="50%" height="24px" />
-                )}
+                    {favorite && <Heart size={16} className="flex-none" />}
+                </div>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                    <h2
+                        className="mb-2 text-lg font-bold truncate group sm:text-xl md:text-2xl lg:text-3xl hover:text-highlight dark:hover:text-highlight-dark md:mb-4"
+                        title={title}>
+                        {no && (
+                            <span className="opacity-60 group-hover:opacity-100">
+                                {no}.{' '}
+                            </span>
+                        )}
+                        {title ? title : <Skeleton width="100%" />}
+                    </h2>
+                </a>
+                <p className="text-base md:text-md lg:text-lg xl:text-xl opacity-60 line-clamp-2">
+                    {subline ? subline : <Skeleton width="50%" />}
+                </p>
             </div>
         </div>
     );
