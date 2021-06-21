787dd79337d32819577c5620f01c885803bc2190
Timo Clasen <goebeltimo@gmail.com>
2021-06-21 17:13:11 +0200

    Add Placeholder for Running Map

--- components/RunningWidget.tsx
+++ components/RunningWidget.tsx
@@ -17,6 +17,11 @@ export function RunningWidget() {
         return <div>Fehler beim Laden…</div>;
     }
 
+    const placeholderLight =
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8+vXTfwAJoAPddP66FgAAAABJRU5ErkJggg==';
+    const placeholderDark =
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPU09X5DwACoAGIYYmEdwAAAABJRU5ErkJggg==';
+
     return (
         <div id="running">
             <WidgetLayout separate>
@@ -31,6 +36,7 @@ export function RunningWidget() {
                             : data?.lastRun?.map?.light
                     }
                     description="Kartenansicht des letzten Laufes von Timo"
+                    blurDataURL={darkMode ? placeholderDark : placeholderLight}
                     unoptimized
                 />
             </WidgetLayout>
