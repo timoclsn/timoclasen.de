d67bcd980af4f66badcd5f05f1b391c5ca292a2f
Timo Clasen <goebeltimo@gmail.com>
2021-10-29 09:05:06 +0200

    Add AVIF Image Format Support

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   experimental: { esmExternals: true },
   images: {
+    formats: ['image/avif', 'image/webp'],
     domains: ['images.ctfassets.net', 'api.mapbox.com', 'i.scdn.co'],
   },
   async rewrites() {
