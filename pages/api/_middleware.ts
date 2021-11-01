77a3bd4a37c7141db6fbfedced7472f09346625c
Timo Clasen <goebeltimo@gmail.com>
2021-11-01 20:10:50 +0100

    Add Blocklist
    Only check secret for certain APIs.

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -5,10 +5,20 @@ const apiSecret = process.env.NEXT_PUBLIC_API_SECRET;
 
 export function middleware(request: NextRequest) {
   const headerKey = request.headers.get('api-secret');
+  const { pathname } = request.nextUrl;
 
-  if (headerKey === apiSecret) {
-    return NextResponse.next();
+  if (
+    pathname.includes('smarthome') ||
+    pathname.includes('control-count') ||
+    pathname.includes('running') ||
+    pathname.includes('now-playing') ||
+    pathname.includes('top-artists') ||
+    pathname.includes('top-tracks')
+  ) {
+    if (headerKey !== apiSecret) {
+      return new Response(null, { status: 401 });
+    }
   }
 
-  return new Response(null, { status: 401 });
+  return NextResponse.next();
 }
