c475d3217fcd630a93c1dab8105707ce73322def
Timo Clasen <goebeltimo@gmail.com>
2022-01-09 21:31:54 +0100

    Update Dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { SWRConfig } from 'swr';
-import { InjectedConnector, Provider, WalletConnectConnector } from 'wagmi';
+import { Provider } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 
 import { ThemeProvider } from '../components/ThemeContext';
 import { ToastProvider } from '../components/ToastProvider';
