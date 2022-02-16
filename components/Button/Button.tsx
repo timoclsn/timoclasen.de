aaf7eab245399600f144d732c02dfbe7c9f64039
Timo Clasen <goebeltimo@gmail.com>
2022-02-16 15:11:33 +0100

    Add CVA

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,37 +1,80 @@
-import clsx from 'clsx';
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
 import type { ReactNode } from 'react';
 import { Children, forwardRef } from 'react';
 
 import buttonStyles from './Button.module.css';
 
-const baseStyles = [
-  'inline-flex',
-  'items-center',
-  'justify-center',
-  'icon-md',
-  'space-x-2',
-  'cursor-pointer',
-  'group-disabled:opacity-50',
-];
+const buttonWrapper = cva(['group', 'focus:outline-none'], {
+  variants: {
+    variant: {
+      solid: [buttonStyles.solidAnimation],
+      link: [buttonStyles.linkAnimation],
+      ghost: [],
+    },
+    size: {
+      normal: [],
+      small: [],
+    },
+    fullWidth: {
+      true: 'w-full',
+    },
+  },
+});
 
-const variantStyles = {
-  solid:
-    'rounded-full text-light bg-highlight dark:bg-highlight-dark group-focus:ring-2 group-focus:ring-inset group-focus:ring-dark dark:group-focus:ring-light',
-  ghost:
-    'rounded-full ring-2 ring-inset ring-dark dark:ring-light group-hover:bg-dark dark:group-hover:bg-light group-hover:bg-opacity-20 dark:group-hover:bg-opacity-20 group-focus:ring-highlight dark:group-focus:ring-highlight-dark',
-  link: 'group-hover:opacity-80 group-focus:underline !p-0',
-};
+const buttonContent = cva(
+  [
+    'inline-flex',
+    'items-center',
+    'justify-center',
+    'icon-md',
+    'space-x-2',
+    'cursor-pointer',
+    'group-disabled:opacity-50',
+  ],
+  {
+    variants: {
+      variant: {
+        solid: [
+          'rounded-full',
+          'text-light',
+          'bg-highlight',
+          'dark:bg-highlight-dark',
+          'group-focus:ring-2',
+          'group-focus:ring-inset',
+          'group-focus:ring-dark',
+          'dark:group-focus:ring-light',
+        ],
+        ghost: [
+          'rounded-full',
+          'ring-2',
+          'ring-inset',
+          'ring-dark',
+          'dark:ring-light',
+          'group-hover:bg-dark',
+          'dark:group-hover:bg-light',
+          'group-hover:bg-opacity-20',
+          'dark:group-hover:bg-opacity-20',
+          'group-focus:ring-highlight',
+          'dark:group-focus:ring-highlight-dark',
+        ],
+        link: ['group-hover:opacity-80', 'group-focus:underline', '!p-0'],
+      },
+      size: {
+        normal: ['px-8', 'py-4', 'font-bold'],
+        small: ['px-4', 'py-2'],
+      },
+      fullWidth: {
+        true: 'w-full',
+      },
+    },
+  }
+);
 
-const sizeStyles = {
-  normal: 'px-8 py-4 font-bold',
-  small: 'px-4 py-2',
-};
+type ButtonProps = VariantProps<typeof buttonContent>;
 
 interface CommmonProps {
   children: ReactNode;
-  variant?: keyof typeof variantStyles;
-  size?: keyof typeof sizeStyles;
-  fullWidth?: boolean;
   title?: string;
   className?: string;
 }
@@ -56,7 +99,7 @@ type ElementProps =
       disabled?: never;
     };
 
-type Props = CommmonProps & ElementProps;
+type Props = CommmonProps & ElementProps & ButtonProps;
 
 export const Button = forwardRef<HTMLButtonElement & HTMLAnchorElement, Props>(
   function Button(
@@ -72,28 +115,21 @@ export const Button = forwardRef<HTMLButtonElement & HTMLAnchorElement, Props>(
     },
     ref
   ) {
-    const wrapperStyles = clsx(
-      'group',
-      'focus:outline-none',
-      fullWidth && 'w-full',
-      variant === 'solid' && buttonStyles.solidAnimation,
-      variant === 'link' && buttonStyles.linkAnimation
-    );
-    const contentStyles = clsx(
-      baseStyles,
-      variantStyles[variant],
-      sizeStyles[size],
-      fullWidth && 'w-full',
-      className
-    );
     return (
       <Element
         type={Element === 'button' ? type : undefined}
         ref={ref}
-        className={wrapperStyles}
+        className={buttonWrapper({ variant, fullWidth })}
         {...props}
       >
-        <span className={contentStyles}>
+        <span
+          className={buttonContent({
+            variant,
+            size,
+            fullWidth,
+            class: className,
+          })}
+        >
           {Children.map(children, (child, index) => (
             <span key={index}>{child}</span>
           ))}
