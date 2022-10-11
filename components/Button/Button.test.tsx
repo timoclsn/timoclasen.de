d81a41b286cd7c962376560f213a52b377331c52
Timo Clasen <goebeltimo@gmail.com>
2022-10-12 00:06:25 +0200

    Fix Types

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, screen } from '../../test-utils';
+import { render, screen } from '../../jest.setup';
 import { Button } from './Button';
 
 describe('<Button />', () => {
