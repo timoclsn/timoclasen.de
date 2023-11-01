06674f46b3326f2d39bb77920127f31dae9cb480
Timo Clasen <goebeltimo@gmail.com>
2023-11-01 22:12:13 +0100

    Fix Plaiceholder Usage

---
+++ lib/placeholder.ts
@@ -0,0 +1,17 @@
+import { getPlaiceholder } from 'plaiceholder';
+
+export const getPlaceholder = async (src: string) => {
+  const buffer = await fetch(src).then(async (res) =>
+    Buffer.from(await res.arrayBuffer()),
+  );
+
+  const {
+    metadata: { height, width },
+    ...plaiceholder
+  } = await getPlaiceholder(buffer, { size: 10 });
+
+  return {
+    ...plaiceholder,
+    img: { src, height, width },
+  };
+};
