59d0bd96929a1a7a1a17dafb33f2667d134b32ad
Timo Clasen <goebeltimo@gmail.com>
2023-11-11 20:14:50 +0100

    Add Typesave Query Content Function

--- lib/content.ts
+++ lib/content.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 const {
   CONTENTFUL_SPACE_ID: spaceId,
   CONTENTFUL_ACCESS_TOKEN: publicAccessToken,
@@ -31,3 +33,37 @@ export async function queryContent(query: string, preview = false) {
   );
   return await res.json();
 }
+
+export const queryContentSave = async <TSchema extends z.ZodTypeAny>(
+  query: string,
+  schema: TSchema,
+) => {
+  let preview = false;
+  const draftContentInDevelopmentMode = true;
+  if (draftContentInDevelopmentMode) {
+    preview = preview || env === 'development';
+  }
+  if (preview) {
+    query = query.replace(/preview: false/g, 'preview: true');
+  }
+
+  const res = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${spaceId}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${
+          preview ? previewAccessToken : publicAccessToken
+        }`,
+      },
+      body: JSON.stringify({ query }),
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+  const data = await res.json();
+
+  return schema.parse(data) as z.infer<TSchema>;
+};
