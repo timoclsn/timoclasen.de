dd983ae507db730cbcf77031af9a316debecf6f8
Timo Clasen <goebeltimo@gmail.com>
2023-11-11 21:24:19 +0100

    Remove Middleware

--- next.config.js
+++ next.config.js
@@ -22,6 +22,18 @@ const config = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 };
 
 module.exports = withBundleAnalyzer(config);
