8006d6295a359caedfa02c38aa7af40fa2f9723c
Timo Clasen <goebeltimo@gmail.com>
2023-11-14 20:44:03 +0100

    Remove unused favicon files and update favicon.svg and manifest.webmanifest

---
+++ app/rss.xml/route.ts
@@ -0,0 +1,56 @@
+import { Feed } from "feed";
+import { getBlogPosts } from "../../data/content";
+import { markdownToHTML } from "../../lib/text";
+
+export const dynamic = "force-dynamic";
+
+const name = "Timo Clasen";
+const email = "timo@timoclasen.de";
+
+export const GET = async (request: Request) => {
+  const url = new URL(request.url);
+  const origin = `${url.protocol}//${url.host}`;
+  const buildUrl = (path: string) => `${origin}${path}`;
+
+  const feed = new Feed({
+    title: `${name} | Blog`,
+    description: "Mein persönlicher Blog",
+    id: buildUrl(""),
+    link: buildUrl(""),
+    language: "de",
+    favicon: `${buildUrl("")}/favicon.png`,
+    copyright: name,
+    feedLinks: {
+      rss: `${buildUrl("")}/rss.xml`,
+    },
+    author: {
+      name: name,
+      email: email,
+      link: buildUrl(""),
+    },
+  });
+  const blogPosts = await getBlogPosts();
+
+  blogPosts.forEach(async (blogPost) => {
+    feed.addItem({
+      title: blogPost.title,
+      id: buildUrl(`/blog/${blogPost.slug}`),
+      link: buildUrl(`/blog/${blogPost.slug}`),
+      content: await markdownToHTML(blogPost.text),
+      author: [
+        {
+          name,
+          email,
+          link: buildUrl(""),
+        },
+      ],
+      date: new Date(blogPost.date),
+    });
+  });
+
+  return new Response(feed.rss2(), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+};
