4547228aaa520dda9816417b6db0e696a78b31e0
Timo Clasen <goebeltimo@gmail.com>
2023-11-15 11:03:23 +0100

    Update dependencies and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     hoverOnlyWhenSupported: true,
   },
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
