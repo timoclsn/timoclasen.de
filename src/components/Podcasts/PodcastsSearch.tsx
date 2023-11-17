8e74f503057877ef7cfc7242c92fdd53593bb30d
Timo Clasen <goebeltimo@gmail.com>
2023-11-17 22:03:33 +0100

    Fix input autofocus in PodcastsSearch component

--- src/components/Podcasts/PodcastsSearch.tsx
+++ src/components/Podcasts/PodcastsSearch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { ChangeEvent } from "react";
 import { Loader, Search as SearchIcon } from "lucide-react";
+import { ChangeEvent } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { useSearchParams } from "../../hooks/useSearchParams";
 import { track } from "../../lib/tracking";
@@ -36,6 +36,7 @@ export const PodcastsSearch = () => {
         placeholder="Podcasts durchsuchen"
         onChange={handleChange}
         autoComplete="off"
+        autoFocus
         className="w-full rounded-xl bg-dark bg-opacity-10 px-4 py-2 text-base placeholder-dark placeholder-opacity-60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-highlight dark:bg-light dark:bg-opacity-10 dark:placeholder-light dark:placeholder-opacity-60 dark:focus-visible:ring-highlight-dark"
       />
       <div className="absolute right-0 top-0 flex h-full items-center justify-center px-4">
