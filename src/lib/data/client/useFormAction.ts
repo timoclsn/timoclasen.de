9949789a81a4538757600b032c2a8a5ba60913b4
Timo Clasen <goebeltimo@gmail.com>
2023-11-23 22:05:50 +0100

    Add More Callbacks

--- src/lib/data/client/useFormAction.ts
+++ src/lib/data/client/useFormAction.ts
@@ -10,28 +10,46 @@ export const useFormAction = <
 >(
   action: ServerFormAction<TInputSchema, TResponse>,
   options: {
+    onRunAction?: () => void;
     onSuccess?: (data: TResponse | null) => void;
     onError?: (errors: {
       error: string | null;
       validationErrors: InferValidationErrors<TInputSchema> | null;
     }) => void;
+    onSettled?: () => void;
   } = {},
 ) => {
   const [state, formAction] = useFormState(action, initalState);
 
+  const proxiedFormAction = new Proxy(formAction, {
+    apply: (target, thisArg, argumentsList) => {
+      options.onRunAction?.();
+      const [payload] = argumentsList;
+      return target.apply(thisArg, [payload]);
+    },
+  });
+
   useEffect(() => {
+    if (state.status === "initial") return;
+
     if (state.status === "success") {
       options.onSuccess?.(state.data);
-    } else if (state.status === "error") {
+    }
+
+    if (state.status === "error" || state.status === "validationError") {
       options.onError?.({
         error: state.error,
         validationErrors: state.validationErrors,
       });
     }
-  }, [options, state.data, state.error, state.status, state.validationErrors]);
+
+    options.onSettled?.();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.status]);
 
   return {
-    runAction: formAction,
+    runAction: proxiedFormAction,
     ...state,
   };
 };
