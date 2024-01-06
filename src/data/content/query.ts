6f299def08975a3c4f706b1f137c26277ff054ff
Timo Clasen <goebeltimo@gmail.com>
2024-01-06 20:10:14 +0100

    Add ISR to CMS Content (#393)
    * Add ISR to CMS Content
    
    * Refactor cache configuration in query.ts
    
    * Add cache key parts and tags to queries

--- src/data/content/query.ts
+++ src/data/content/query.ts
@@ -8,6 +8,17 @@ import { createQuery } from "../clients";
 
 export const metadata = createQuery({
   input: z.string(),
+  cache: ({ input }) => {
+    const slug = input;
+    const tag = `metadata-${slug}`;
+    return {
+      keyParts: [tag],
+      options: {
+        revalidate: 60,
+        tags: [tag],
+      },
+    };
+  },
   query: async ({ input: slug }) => {
     const response = await queryContent(
       `{
@@ -40,6 +51,17 @@ export const metadata = createQuery({
 
 export const image = createQuery({
   input: z.string(),
+  cache: ({ input }) => {
+    const title = input;
+    const tag = `image-${title}`;
+    return {
+      keyParts: [tag],
+      options: {
+        revalidate: 60,
+        tags: [tag],
+      },
+    };
+  },
   query: async ({ input: title }) => {
     const response = await queryContent(
       `{
@@ -69,6 +91,13 @@ export const image = createQuery({
 });
 
 export const person = createQuery({
+  cache: {
+    keyParts: ["person"],
+    options: {
+      revalidate: 60,
+      tags: ["person"],
+    },
+  },
   query: async () => {
     const response = await queryContent(
       `{
@@ -127,6 +156,17 @@ export const person = createQuery({
 
 export const textSnippet = createQuery({
   input: z.string(),
+  cache: ({ input }) => {
+    const title = input;
+    const tag = `text-snippet-${title}`;
+    return {
+      keyParts: [tag],
+      options: {
+        revalidate: 60,
+        tags: [tag],
+      },
+    };
+  },
   query: async ({ input: title }) => {
     const response = await queryContent(
       `{
@@ -154,6 +194,13 @@ export const textSnippet = createQuery({
 });
 
 export const blogPosts = createQuery({
+  cache: {
+    keyParts: ["blogposts"],
+    options: {
+      revalidate: 60,
+      tags: ["blogposts"],
+    },
+  },
   query: async () => {
     const response = await queryContent(
       `{
@@ -209,6 +256,17 @@ export const blogPosts = createQuery({
 
 export const blogPost = createQuery({
   input: z.string(),
+  cache: ({ input }) => {
+    const slug = input;
+    const tag = `blogpost-${slug}`;
+    return {
+      keyParts: [tag],
+      options: {
+        revalidate: 60,
+        tags: [tag],
+      },
+    };
+  },
   query: async ({ input: slug }) => {
     const response = await queryContent(
       `{
@@ -290,6 +348,13 @@ export const blogPost = createQuery({
 });
 
 export const cvEntries = createQuery({
+  cache: {
+    keyParts: ["cv-entries"],
+    options: {
+      revalidate: 60,
+      tags: ["cv-entries"],
+    },
+  },
   query: async () => {
     const response = await queryContent(
       `{
@@ -337,6 +402,13 @@ export const cvEntries = createQuery({
 export type Photo = Awaited<ReturnType<typeof photos>>[number];
 
 export const photos = createQuery({
+  cache: {
+    keyParts: ["photos"],
+    options: {
+      revalidate: 60,
+      tags: ["photos"],
+    },
+  },
   query: async () => {
     const response = await queryContent(
       `{
