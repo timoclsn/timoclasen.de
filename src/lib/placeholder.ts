e4f498dedf3449d76030e6f2c150ed79538da73b
Timo Clasen <goebeltimo@gmail.com>
2024-08-03 21:36:42 +0200

    Disable Caching

--- src/lib/placeholder.ts
+++ src/lib/placeholder.ts
@@ -1,11 +1,9 @@
 import { getPlaiceholder } from "plaiceholder";
 
 export const getPlaceholder = async (src: string) => {
-  const buffer = await fetch(src, {
-    next: {
-      revalidate: 60,
-    },
-  }).then(async (res) => Buffer.from(await res.arrayBuffer()));
+  const buffer = await fetch(src).then(async (res) =>
+    Buffer.from(await res.arrayBuffer()),
+  );
 
   const {
     metadata: { height, width },
