387af874f81348ee67a2f7e80ba6fc48ec395dd8
Timo Clasen <goebeltimo@gmail.com>
2024-08-16 21:29:19 +0200

    Add BaseError Class

--- src/lib/data/errors.ts
+++ src/lib/data/errors.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_ERROR_MESSAGE = 'Something went wrong.';
+export const DEFAULT_ERROR_MESSAGE = "Something went wrong.";
 
 export class ActionError extends Error {
   public override readonly cause?: Error;
@@ -6,9 +6,36 @@ export class ActionError extends Error {
 
   constructor(opts: { message: string; log?: string; cause?: Error }) {
     super(opts.message);
-    this.name = 'ActionError';
+    this.name = "ActionError";
     this.message = opts.message;
     this.log = opts.log;
     this.cause = opts.cause;
+  }
+}
+
+export type ErrorContext = Record<string, unknown>;
+
+export abstract class BaseError<
+  TContext extends ErrorContext = ErrorContext,
+> extends Error {
+  public readonly context?: TContext;
+
+  constructor(
+    opts: {
+      message?: string;
+      cause?: Error;
+      context?: TContext;
+    } = {},
+  ) {
+    super(opts.message, {
+      cause: opts.cause,
+    });
+    this.context = opts.context;
+  }
+
+  public toString() {
+    return `${this.name}: ${this.message} - ${JSON.stringify(
+      this.context,
+    )} - caused by ${this.cause?.toString()}`;
   }
 }
