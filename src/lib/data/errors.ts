bdc9bc2bf000a0a53facb9088b639b636c07e270
Timo Clasen <goebeltimo@gmail.com>
2024-08-25 20:06:26 +0200

    Add Comments

--- src/lib/data/errors.ts
+++ src/lib/data/errors.ts
@@ -1,5 +1,6 @@
 //
 // Action Error
+// Use this error in actions on the server to pass the error message directly to the ui.
 //
 
 export const DEFAULT_ACTION_ERROR_MESSAGE = "Etwas ist schiefgelaufen…";
@@ -21,10 +22,14 @@ export const isActionError = (error: unknown): error is ActionError =>
 
 //
 // Query Error
+// Use this error in queries on the server to pass the error message directly to the ui.
 //
 
 export const DEFAULT_QUERY_ERROR_MESSAGE = "Fehler beim Laden…";
 const QUERY_ERROR_NAME = "QueryError";
+
+// When a query error is thrown, the message is the only value that is passed to the error boundary.
+// To provide more context to the error boundary (detecting a QueryError), we prefix the message with the error name.
 const QUERY_ERROR_PREFIX = `${QUERY_ERROR_NAME}:`;
 
 export class QueryError extends Error {
