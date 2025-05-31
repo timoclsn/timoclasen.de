b935ddf66e28413f6f947a9d8c841a6e546d3741
Timo Clasen <goebeltimo@gmail.com>
2025-05-31 22:18:09 +0200

    Update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:cypress/recommended",
+    "prettier",
+  ),
+];
+
+export default eslintConfig;
